fe1ba7e407719eb63d5fef3f2ee10fa512ccceb8
Davishisaki <142029074+Davishisaki@users.noreply.github.com>
2025-02-10 03:01:37 -0300

    Código fonte e banco de dados

---
+++ chatbot_paranagua_base.pl
@@ -0,0 +1,20 @@
+:- use_module(library(odbc)).
+
+% Configuração do banco de dados
+connect_to_db :-
+    odbc_connect('MySQLDSN', _Connection, [user('chatbotpgua'), password('chatbotpgua'), alias(mydb), open(once)]).
+
+% Função para consultar dados e usar assertz
+consult_and_assert :-
+    connect_to_db,
+    odbc_query(mydb, 'SELECT pergunta, resposta FROM pergunta_resposta', row(Pergunta, Resposta)),
+    assertz(pergunta_resposta(Pergunta, Resposta)),
+    fail; % Força a falha para continuar a busca por mais resultados
+    odbc_disconnect(mydb).
+
+% Exemplo de uso
+exemplo :-
+    consult_and_assert,
+    % Exibir todos os fatos adicionados
+    %  forall(pergunta_resposta(Pergunta, Resposta), (write('Pergunta: '), write(Pergunta), write(', Resposta: '), write(Resposta), nl)).
+    forall(pergunta_resposta(Pergunta, Resposta), (write('pergunta_resposta('), write(Pergunta),write(', '), write(Resposta),write(').'), nl)).
